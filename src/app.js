f22a3ad9b101f44ba8e66069027d323d37417322
Rabia <rabia@Rabias-MacBook-Air.local>
2022-01-26 23:23:53 -0500

    added changes for share target

--- src/app.js
+++ src/app.js
@@ -9,3 +9,12 @@ document.querySelector('#share').addEventListener('click', () => {
       .catch((error) => console.log('Error sharing', error));
   }
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+  const parsedUrl = new URL(window.location);
+  // searchParams.get() will properly handle decoding the values.
+  console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+  console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+  console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+  alert(parsedUrl.searchParams.get('title'));
+});
