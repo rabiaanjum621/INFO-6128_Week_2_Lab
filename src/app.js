78a88ed2a867edada023947a3dd1120bf0ee0a3f
Rabia <rabia@Rabias-MacBook-Air.local>
2022-01-27 00:57:26 -0500

    added condition

--- src/app.js
+++ src/app.js
@@ -13,8 +13,12 @@ document.querySelector('#share').addEventListener('click', () => {
 window.addEventListener('DOMContentLoaded', () => {
   const parsedUrl = new URL(window.location);
   // searchParams.get() will properly handle decoding the values.
+  if(parsedUrl.searchParams.get('title') != null){
   console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
   console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
   console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
   alert(parsedUrl.searchParams.get('title'));
+  }
+  else
+  alert('null value received');
 });
